2c260ea18d1924f139894677e0c56345a1cf93fa
x674 <iron-nmen@ya.ru>
2022-06-21 13:55:51 +1000

    Complete task

--- src/main/java/com/x674/App.java
+++ src/main/java/com/x674/App.java
@@ -1,14 +1,17 @@
 package com.x674;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.MessageFormat;
 import java.util.Scanner;
 
 public class App {
+
+    public static Scanner input = new Scanner(System.in);
+    
     // JDBC driver name and database URL
     static final String JDBC_DRIVER = "org.h2.Driver";
     static final String DB_URL = "jdbc:h2:mem:StudentsDataBase";
@@ -23,29 +26,32 @@ public class App {
             " middleName VARCHAR(255), " +
             " birthday DATETIME, " +
             " PRIMARY KEY ( id ))";
-     private static final String allStrudentsQuery = "select * from students";
-    //private static final String addStrudentQuery = "INSERT INTO students " + "VALUES (100, 'Zara', 'Ali', 18)";
-    private static final String removeStrudentQuery = "DELETE FROM Registration " + "WHERE id = 101";
 
-    private static void addStudent(Statement statement, int id, String lastName, String firstName, String middleName, String birthday) {
+    private static final String allStrudentsQuery = "select * from students";
+
+    private static void addStudent(Statement statement, int id, String lastName, String firstName, String middleName,
+            String birthday) {
         try {
-            String sql =
-
-            "INSERT INTO Registration " + "VALUES("+id+
-                    ", "+lastName+"'"+
-                    ", '"+firstName+"'"+
-                    ", '"+middleName+"'"+
-                    birthday+
-                    ", '"+lastName+"'"
-                    +")";
-
+            String sql = "INSERT INTO STUDENTS " + "VALUES(" + id +
+                    ", '" + lastName + "'" +
+                    ", '" + firstName + "'" +
+                    ", '" + middleName + "'" +
+                    ", '" + birthday + "'"
+                    + ")";
             statement.executeUpdate(sql);
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-
+    private static void removeStudent(Statement statement, int id) {
+        try {
+            String sql = "DELETE FROM STUDENTS " + "WHERE ID=" + id;
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
 
         Connection conn = null;
@@ -55,35 +61,53 @@ public class App {
             Class.forName(JDBC_DRIVER);
 
             // STEP 2: Open a connection
-            System.out.println("Connecting to database...");
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
 
             // STEP 3: Execute a query
-            System.out.println("Creating table in given database...");
             stmt = conn.createStatement();
             stmt.executeUpdate(createTableQuery);
-            switch (menu()) {
-                case 1:
-                    addStudent(stmt, 100, "Pupkin", "Vasua", "Vasilewich", "01.01.2000");
-                    break;
-                case 2:
-                    stmt.executeUpdate(removeStrudentQuery);
-                    break;
-                case 3:
-                    ResultSet rs = stmt.executeQuery(allStrudentsQuery);
-                    while (rs.next())
-                    {
-                        System.out.println(rs.getString("firstName") + " " + rs.getString("middleName") + " " + rs.getString("lastName"));
-                    }
-                    break;
-                case 4:
-                    System.out.println("Goodbye!");
-                    // STEP 4: Clean-up environment
-                    stmt.close();
-                    conn.close();
-                    break;
-                default:
-                    // The user input an unexpected choice.
+            while (true) {
+                switch (menu()) {
+                    case 1:
+                        System.out.println("Введите id студента");
+                        int id = input.nextInt();
+                        System.out.println("Введите фамилию студента");
+                        String lastName = input.next();
+                        System.out.println("Введите имя студента");
+                        String firstName = input.next();
+                        System.out.println("Введите отчество студента");
+                        String middleName = input.next();
+                        System.out.println("Введите дату рождения студента (2000-12-25)");
+                        String birthday = input.next();
+
+                        addStudent(stmt, id, lastName, firstName, middleName, birthday);
+                        break;
+                    case 2:
+                        System.out.println("Введите id студента");
+                        int idStudent = input.nextInt();
+                        removeStudent(stmt, idStudent);
+                        break;
+                    case 3:
+                        ResultSet rs = stmt.executeQuery(allStrudentsQuery);
+                        clearConsole();
+                        while (rs.next()) {
+                            System.out.println(rs.getString("id") + " " + rs.getString("firstName") + " " +
+                                    rs.getString("middleName") + " " + rs.getString("lastName") + " " +
+                                    rs.getString("birthday"));
+                        }
+                        System.out.println("Нажмите любую клавишу чтобы продолжить...");
+                        System.in.read();
+                        break;
+                    case 4:
+                        System.out.println("Goodbye!");
+                        // STEP 4: Clean-up environment
+                        stmt.close();
+                        conn.close();
+                        return;
+                    default:
+
+                }
+                clearConsole();
             }
 
         } catch (SQLException se) {
@@ -105,16 +129,12 @@ public class App {
             } catch (SQLException se) {
                 se.printStackTrace();
             } // end finally try
-        } // end try
+        }
 
     }
 
     public static int menu() {
-
         int selection;
-        Scanner input = new Scanner(System.in);
-
-        /***************************************************/
 
         System.out.println("Выбирите действие");
         System.out.println("-------------------------\n");
@@ -125,5 +145,16 @@ public class App {
 
         selection = input.nextInt();
         return selection;
+    }
+
+    public final static void clearConsole() {
+        // Clears Screen in java
+        try {
+            if (System.getProperty("os.name").contains("Windows"))
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            else
+                Runtime.getRuntime().exec("clear");
+        } catch (IOException | InterruptedException ex) {
+        }
     }
 }
